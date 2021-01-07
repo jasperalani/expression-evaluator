4d88a9f1bffc2d2cf4cda26891b22a49e26f7740
Jasper Alani <jasperalani@gmail.com>
2021-01-07 00:43:11 +0000

    Preordered equations now evaluate correctly

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simple-equation-solver
 A simple equation solver (eg. 8 * 7 / (6+5) - 4 = 1.09 )
+
+Currently works if equations are already in correct order of operations.
+That is next to implement.
--- solver.v
+++ solver.v
@@ -1,30 +1,141 @@
 import os
+import strconv
+
+struct Term {
+	mut:
+	tokens []string
+	is_operator bool
+}
+
+struct Operation {
+	mut:
+	left int
+	right int
+	operator int
+}
 
 fn main() {
 
 	input_equation := os.input('Enter your equation: ')
 
-	operations := ["*", "/", "+", "-"]
+	terms := get_term_list(input_equation)
+
+	operation_queue := gen_operation_queue(terms)
+
+	result := execute_operation_queue(operation_queue)
+
+	println(result)
+
+}
+
+fn execute_operation_queue (queue [][]Operation) f64 {
+	mut op_output := 0.0
+
+	// Execute operation queue
+	for _, tier in queue {
+		for op_index, operation in tier {
+
+			match operation.operator {
+				0 {
+					if op_index == 0 {
+						op_output = operation.left * operation.right
+					}else{
+						op_output = op_output * operation.right
+					}
+				}
+				1 {
+					op_output = op_output / operation.right
+				}
+				2 {
+					op_output = op_output + operation.right
+				}
+				3 {
+					op_output = op_output - operation.right
+				}
+				else {}
+			}
+
+		}
+	}
+
+	return op_output
+}
+
+fn gen_operation_queue(terms []Term) [][]Operation {
+
+	mut tiered_operation_queue := [][]Operation{len: 4, init: []Operation{}}
+
+	// Construct operation queue
+	for index, term in terms {
+		if !term.is_operator {
+			continue
+		}
+
+		mut operator_operation_index := 0
+
+		match term.tokens[0] {
+			'/' {
+				operator_operation_index = 1
+			}
+			'+' {
+				operator_operation_index = 2
+			}
+			'-' {
+				operator_operation_index = 3
+			}
+			else {}
+		}
+
+		tiered_operation_queue[operator_operation_index] << Operation{
+			left: evaluate_numbers(terms[index-1].tokens)
+			right: evaluate_numbers(terms[index+1].tokens)
+			operator: operator_operation_index
+			}
+
+	}
+
+	return tiered_operation_queue
+
+}
+
+fn evaluate_numbers(tokens []string) int {
+	mut return_number := 0
+	for index, token in tokens {
+		mut number := strconv.atoi(token) or {
+			return 0
+		}
+
+		zeros := tokens.len - index
+		for i := 0; i < zeros-1; i++ {
+			number = number * 10
+		}
+		
+		return_number += number
+	}
+	return return_number
+}
+
+fn get_term_list (equation string) []Term {
+	operators := ["*", "/", "+", "-"]
 	numbers := ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-	tokens := gen_token_list(input_equation)
+	tokens := gen_token_list(equation)
 	mut working_tokens_list := tokens.clone()
 
 	mut terms := []Term{}
 	mut working_term := Term{}
 
 	mut used_term_indexes := []int{}
-
 	mut used_index_offset := 0
 
 	outer: for index, token in working_tokens_list {
 		
-		if 0 == index && in_array_str(token, operations) {
+		if 0 == index && in_array_str(token, operators) {
 			eprintln("First character cannot be an operation.")
 			exit(1)
 		}
 		
-		if !in_array_str(token, operations) && !in_array_str(token, numbers) {
+		if !in_array_str(token, operators) && !in_array_str(token, numbers) {
 			eprintln("Equation can only contain numbers or one of: * / + -") 
 			exit(1)
 		}
@@ -33,12 +144,13 @@ fn main() {
 			continue
 		}
 
-		if 0 != index && in_array_str(token, operations) {
+		if 0 != index && in_array_str(token, operators) {
 			working_term = Term{}
 			working_term.tokens << token
+			working_term.is_operator = true
 			// term is complete, append to array
 			terms << working_term
-			// reset working term
+			// reset working term 
 			working_term = Term{}
 			used_term_indexes << index
 			used_index_offset++
@@ -50,7 +162,7 @@ fn main() {
 			working_term = Term{} 
 			working_term.tokens << token
 
-			if input_equation.len == 1 {
+			if equation.len == 1 {
 				continue
 			}
 
@@ -63,7 +175,7 @@ fn main() {
 			// find the rest of the number
 			for sub_index, sub_token in tokens[index+1..tokens.len] {
 
-				if !in_array_str(sub_token, operations) {
+				if !in_array_str(sub_token, operators) {
 					if used_term_indexes.len > 0 {
 						used_term_indexes << used_term_indexes[used_term_indexes.len-1]+1
 					}else{
@@ -79,21 +191,20 @@ fn main() {
 
 			// term is complete, append to array
 			terms << working_term
-			// // reset working term
+			// reset working term
 			working_term = Term{} 
 
 		}
 
-	}
+	} 
 
-	println(terms)
-	
+	return terms
 }
 
-fn gen_token_list (input string) []string {
+fn gen_token_list (equation string) []string {
 	mut tokens := []string{}
-	for index, _ in input {
-		tokens << input[index..index+1]
+	for index, _ in equation {
+		tokens << equation[index..index+1]
 	}
 	return tokens
 }
@@ -116,9 +227,4 @@ fn in_array_int(needle int, haystack []int) bool {
 	}
 
 	return false
-}
-
-struct Term {
-	mut:
-	tokens []string
 }
